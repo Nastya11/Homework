9d2d71ed1dc2a7ed57529bd1508e4bd3ff14515a
Nastya11 <cat110399@gmail.com>
2018-02-06 22:03:06 +0300

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -1,29 +1,40 @@
 import math
 import cmath
 
-def vietas_solution(b,c): #решение с помощью теоремы виета
-    d = b**2 - 4*c
-    if d >= 0:
+def vietas_solution(b,c): #СЂРµС€РµРЅРёРµ С‡РµСЂРµР· С‚РµРѕСЂРµРјСѓ Р’РёРµС‚Р°
+    if b==0 and c==0:
+      x=0
+      return x
+    else:
+      d = b**2 - 4*c
+      if d >= 0:
         sqd = math.sqrt(d)
         if b>= 0:
             x1 = (-b - sqd) / 2.0
         else:
             x1 = (-b + sqd) / 2.0
         x2 = c/x1
-    else:
+      else:
         sqd = cmath.sqrt(d)
         x1 = (-b + sqd) / 2.0
         x2 = (-b - sqd) / 2.0
-    return x1, x2
-def machineEpsilon(e): #ищет машинный эпсилон (понадобится для функции taylor_solution)
+      return x1, x2
+def machineEpsilon(e): #СЃС‡РёС‚Р°РµС‚ РјР°С€РёРЅРЅС‹Р№ СЌРїСЃРёР»РѕРі (РЅСѓР¶РЅРѕ РґР»СЏ taylor_solution)
     e = 1.0
     while (1.0 + e/2.0) > 1.0:
         e = e/2.0
     return e
-def taylor_solution(b,c): #решение с помощью разложения в ряд
-    if b**2 - 4*c < 0:
+def taylor_solution(b,c): #СЂРµС€РµРЅРёРµ С‡РµСЂРµР· РґРёСЃРєСЂРёРјРёРЅР°РЅС‚ РёР»Рё СЂР°Р·Р»РѕР¶РµРЅРёРµ РІ СЂСЏРґ РўРµР№Р»РѕСЂР° РІ РїР»РѕС…РѕРј СЃР»СѓС‡Р°Рµ
+    if b==0 and c==0:
+      x=0
+      return x
+    elif b==0:
+      x1,x2 = vietas_solution(b,c)
+      return x1, x2
+    else:  
+      if b**2 - 4*c < 0:
         x1,x2 = vietas_solution(b,c)
-    else:
+      else:
         if 4*c/b**2 < machineEpsilon(1.0)**2:
             x1 = - b
             x2 = - c / b - (4 * c / b) ** 2 / 16
@@ -31,11 +42,14 @@ def taylor_solution(b,c): #решение с помощью разложения в ряд
             D = b ** 2 - 4 * c
             x1 = (- b + D ** 0.5) / 2
             x2 = (- b - D ** 0.5) / 2
-    return x1, x2
+      return x1, x2
   
-  #вывод
+  #РІС‹РІРѕРґ
 print("b=4, c=3 \n vieta's formulas: ", vietas_solution(4.0,3.0)," \n Discriminant/taylor series: ",taylor_solution(4.0,3.0 ),"\n")
 print("b=2, c=1 \n  vieta's formulas: ",vietas_solution(2.0,1.0), " \n Discriminant/taylor series: ",taylor_solution(2.0,1.0), "\n")
 print("b=0.5, c=4 \n vieta's formulas: ",vietas_solution(0.5,4.0), " \n Discriminant/taylor series: ",taylor_solution(0.5,4.0), "\n")
 print("b=10^20, c=3 \n vieta's formulas: ",vietas_solution(1e20,3.0)," \n Discriminant/taylor series: ",taylor_solution(1e20,3.0),"\n")
-print("b=-10^20, c=4 \n vieta's formulas: ",vietas_solution(-1e20,4.0)," \n Discriminant/taylor series: ",taylor_solution(-1e20,4.0))
+print("b=-10^20, c=4 \n vieta's formulas: ",vietas_solution(-10^20,4)," \n Discriminant/taylor series: ",taylor_solution(-10^20,4),"\n")
+print("b=0, c=0 \n vieta's formulas: ",vietas_solution(0,0)," \n Discriminant/taylor series: ",taylor_solution(0,0),"\n")
+print("b=0, c=4 \n vieta's formulas: ",vietas_solution(0,-4)," \n Discriminant/taylor series: ",taylor_solution(0,-4),"\n")
+print("b=1, c=0 \n vieta's formulas: ",vietas_solution(1,0)," \n Discriminant/taylor series: ",taylor_solution(1,0),"\n")
